617d56ee3e5b3ce08519d926adc8b9ee92af0f47
Daifyyy <denis.honzejk@gmail.com>
2025-08-20 19:52:42 +0200

    Handle pandas NA in expected points

--- tests/test_safe_stat_pd_na.py
+++ tests/test_safe_stat_pd_na.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from utils.poisson_utils.team_analysis import (
     expected_goals_combined_homeaway_allmatches,
+    calculate_expected_and_actual_points,
 )
 
 
@@ -24,3 +25,26 @@ def test_expected_goals_handles_pandas_na():
 
     assert isinstance(home, float)
     assert isinstance(away, float)
+
+
+def test_expected_points_handles_pandas_na():
+    df = pd.DataFrame(
+        {
+            "Date": ["2024-01-01"],
+            "HomeTeam": ["Existing"],
+            "AwayTeam": ["Other"],
+            "FTHG": [1],
+            "FTAG": [1],
+            "HS": [pd.NA],
+            "HST": [pd.NA],
+            "AS": [pd.NA],
+            "AST": [pd.NA],
+        }
+    )
+
+    res = calculate_expected_and_actual_points(df)
+
+    assert res["Existing"]["points"] == 1
+    assert res["Other"]["points"] == 1
+    assert isinstance(res["Existing"]["expected_points"], float)
+    assert isinstance(res["Other"]["expected_points"], float)
