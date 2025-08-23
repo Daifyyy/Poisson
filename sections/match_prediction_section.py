5393369e95072b0ccd1eca4169093ae0edb85100
Daifyyy <denis.honzejk@gmail.com>
2025-08-23 23:20:24 +0200

    Wrap RandomForest with RandomizedSearchCV and save best params

--- sections/match_prediction_section.py
+++ sections/match_prediction_section.py
@@ -66,7 +66,7 @@ from utils.ml.random_forest import (
 def get_rf_model():
     return load_model()
 
-RF_MODEL, RF_FEATURE_NAMES, RF_LABEL_ENCODER = get_rf_model()
+RF_MODEL, RF_FEATURE_NAMES, RF_LABEL_ENCODER, _ = get_rf_model()
 
 @st.cache_data
 def load_upcoming_xg() -> pd.DataFrame:
